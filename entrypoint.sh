52cf639e0a1e53fb5a62315aae3989f63e469ad9
Bektur <bekturkochorbaev64@gmail.com>
2025-10-29 14:27:55 +0600

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
-set -e
-
-# Ожидаем, пока PostgreSQL будет готов к подключению
-echo "⏳ Waiting for PostgreSQL to be ready..."
-until nc -z db 5432; do
-  sleep 1
-done
-echo "✅ PostgreSQL is ready!"
 
 # Выполняем миграции Django
-echo "📦 Running migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python manage.py makemigrations theatre tickets --noinput
+python manage.py migrate theatre tickets --noinput
 
 # Собираем статические файлы
-echo "📁 Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
-# Запускаем бота (в фоне)
-echo "🤖 Starting bot..."
+# Запускаем бот
 python accounts/bot.py &
 
 # Запускаем Gunicorn
-echo "🚀 Starting Gunicorn..."
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --timeout 600
